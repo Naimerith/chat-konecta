c93a32f01df68d4a5e1aad264914e5b9783d6c82
Naimerith <naimerith89@gmail.com>
2022-07-11 21:07:04 -0500

    utlimas modificaciones

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,21 +3,16 @@ import { Avatar } from '@material-ui/core';
 import '../styles-sheets/ScreenChat.css'
 
 const Message = (props) => {
-    return (
-        <div className='container-screem-chat'>
-            <div
-                className='msg'>
-                <Avatar />
-                <div className='container-msg'>
-                    <h4>Naimerith</h4>
-                    {/* <h5 className='messsage-date'>
-                Fecha y Hora: {new Date(msg.id).toLocaleString}
-              </h5> */}
-                    <p>{props.msg}</p>
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div
+      className='msg'>
+      <Avatar />
+      <div className='container-msg'>
+        <h4>Naimerith</h4>
+        <p>{props.msg}</p>
+      </div>
+    </div>
+  )
 }
 
-export default Message
+export default Message;
